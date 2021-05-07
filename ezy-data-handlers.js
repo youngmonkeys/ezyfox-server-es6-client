6d4c09ca324c8a37b8bd6699d9b29ee84baf2402
vu-luong <vubinhcht@gmail.com>
2021-05-07 11:52:11 +1000

    Add comments for ezy-data-handlers

--- ezy-data-handlers.js
+++ ezy-data-handlers.js
@@ -2,34 +2,98 @@ import Const from './ezy-constants'
 import Util from './ezy-util'
 import Entity from './ezy-entities'
 
+/**
+ * A listener which fires when client receives Const.EzyCommand.HANDSHAKE command from server.
+ * After that, it automatically sends a login request to server with username
+ * and password returned from getLoginRequest()
+ * - Requirement: getLoginRequest() need to be implemented
+ * - Usage:
+ * ```
+ *      let handshakeHandler = new Ezy.HandshakeHandler();
+ *      handshakeHandler.getLoginRequest = function () {
+ *          let username = "test";
+ *          let password = "test1234";
+ *              return ["freechat", username, password, []];
+ *      };
+ *
+ *      let loginSuccessHandler = new Ezy.LoginSuccessHandler();
+ *      loginSuccessHandler.handleLoginSuccess = function () {
+ *          // What to do when login successfully login
+ *      };
+ *
+ *      let loginErrorHandler = new Ezy.LoginErrorHandler();
+ *      loginErrorHandler.handleLoginError = function (event) {
+ *          // What to do when login unsuccessfully
+ *      };
+ *
+ *      // connect to server
+ *      client.connect(url);
+ * ```
+ */
 export class EzyHandshakeHandler {
 
+    /**
+     * Automatically call this function when client receives HANDSHAKE command
+     * from server.
+     * @param data [encryptedServerPublicKey, token, sessionId]
+     */
 	handle(data) {
 		this.startPing();
 		this.handleLogin();
 		this.postHandle(data);
 	}
 
+    /**
+     * This function need to be manually defined to specify
+     * what to do after sending login request
+     * @param data: [encryptedServerPublicKey, token, sessionId]
+     */
 	postHandle(data) {
     }
 
+    /**
+     * Send loginRequest to server
+     */
 	handleLogin() {
 		var loginRequest = this.getLoginRequest();
 		this.client.sendRequest(Const.EzyCommand.LOGIN, loginRequest);
     }
-    
+
+    /**
+     * This function need to be manually defined to specify
+     * needed login information
+     * @returns [zonename, username, password, data]
+     */
     getLoginRequest() {
         return ["test", "test", "test", []];
 	}
-	
+
+    /**
+     * Start sending ping request
+     */
 	startPing() {
 		this.client.pingSchedule.start();
 	}
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.LOGIN
+ * command from server, meaning that login is successful.
+ * - Usage:
+ * ```
+ *      let loginSuccessHandler = new Ezy.LoginSuccessHandler();
+ *      loginSuccessHandler.handleLoginSuccess = function () {
+ *          // What to do when login successfully login
+ *      };
+ * ```
+ */
 export class EzyLoginSuccessHandler {
+    /**
+     * Automatically call this function when client receives LOGIN command
+     * from server
+     * @param data [zoneId, zoneName, userId, username, responseData=null]
+     */
     handle(data) {
         var zoneId = data[0];
         var zoneName = data[1];
@@ -41,29 +105,69 @@ export class EzyLoginSuccessHandler {
         var user = new Entity.EzyUser(userId, username);
         this.client.me = user;
         this.client.zone = zone;
-        this.handleLoginSuccess(responseData);   
+        this.handleLoginSuccess(responseData);
         Util.EzyLogger.console("user: " + user.name + " logged in successfully");
     }
 
+    /**
+     * This function need to be manually defined to specify what to
+     * do after successful login
+     * @param responseData Additional data received from server
+     */
     handleLoginSuccess(responseData) {
     }
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.LOGIN_ERROR
+ * command from server, meaning that login is unsuccessfully.
+ * - Usage
+ * ```
+ *      let loginErrorHandler = new Ezy.LoginErrorHandler();
+ *      loginErrorHandler.handleLoginError = function (event) {
+ *          // What to do when login unsuccessfully
+ *      };
+ * ```
+ */
 export class EzyLoginErrorHandler {
+    /**
+     * Automatically call this function when client receives LOGIN_ERROR command
+     * from server
+     * @param data [errorId, errorMessage]
+     */
     handle(data) {
         this.client.disconnect(401);
         this.handleLoginError(data);
     }
 
+    /**
+     * This function need to be manually defined to specify what to
+     * do after unsuccessful login
+     * @param data [errorId, errorMessage]
+     */
     handleLoginError(data) {
     }
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.APP_ACCESS command
+ * from server, meaning that the client is allowed to access the app. This is the
+ * response from server after client sends APP_ACCESS request
+ * - Usage:
+ * ```
+ *      let accessAppHandler = new Ezy.AppAccessHandler();
+ *      accessAppHandler.postHandle = function (app, data) {
+ *          // What to do after being allowed to access app
+ *      };
+ */
 export class EzyAppAccessHandler {
+    /**
+     * Automatically call this function when client receives APP_ACCESS command
+     * from server
+     * @param data [appId, appName, data=[]]
+     */
     handle(data) {
         var zone = this.client.zone;
         var appManager = zone.appManager;
@@ -73,6 +177,12 @@ export class EzyAppAccessHandler {
         Util.EzyLogger.console("access app: " + app.name + " successfully");
     }
 
+    /**
+     * Create an EzyApp entity for client
+     * @param zone {EzyZone}
+     * @param data {array} [appId, appName, data=[]]
+     * @returns {EzyApp} Created app
+     */
     newApp(zone, data) {
         var appId = data[0];
         var appName = data[1];
@@ -80,13 +190,33 @@ export class EzyAppAccessHandler {
         return app;
     }
 
+    /**
+     * This function need to be manually defined to specify what to
+     * do after server allows client to access the app
+     * @param app {EzyApp}
+     * @param data {array}
+     */
     postHandle(app, data) {
     }
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.APP_EXIT command
+ * from server, meaning that server tells client to exit the app
+ * - Usage:
+ * ```
+ *      let exitAppHandler = new Ezy.EzyAppExitHandler();
+ *      exitAppHandler.postHandle = function (app, data) {
+ *          // What to do after exiting the app
+ *      };
+ */
 export class EzyAppExitHandler {
+    /**
+     * Automatically call this function when client receives APP_EXIT command
+     * from server
+     * @param data {array} [appId, reasonId]
+     */
     handle(data) {
         var zone = this.client.zone;
         var appManager = zone.appManager
@@ -99,6 +229,12 @@ export class EzyAppExitHandler {
         }
     }
 
+    /**
+     * This function need to be manually defined to specify what to
+     * do after server allows client to access the app
+     * @param app {EzyApp} App has just been removed
+     * @param data {array} [appId, reasonId]
+     */
     postHandle(app, data) {
     }
 
@@ -112,7 +248,7 @@ export class EzyAppResponseHandler {
         var responseData = data[1];
         var cmd = responseData[0];
         var commandData = responseData[1];
-        
+
         var app = this.client.getAppById(appId);
         if(!app) {
             Util.EzyLogger.console("receive message when has not joined app yet");

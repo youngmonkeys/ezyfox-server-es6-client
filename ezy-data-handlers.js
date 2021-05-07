2cff59769a543741da5285b1a50322666a05b629
vu-luong <vubinhcht@gmail.com>
2021-05-07 13:16:45 +1000

    Add comments for ezy-data-handlers

--- ezy-data-handlers.js
+++ ezy-data-handlers.js
@@ -161,6 +161,7 @@ export class EzyLoginErrorHandler {
  *      accessAppHandler.postHandle = function (app, data) {
  *          // What to do after being allowed to access app
  *      };
+ * ```
  */
 export class EzyAppAccessHandler {
     /**
@@ -193,8 +194,8 @@ export class EzyAppAccessHandler {
     /**
      * This function need to be manually defined to specify what to
      * do after server allows client to access the app
-     * @param app {EzyApp}
-     * @param data {array}
+     * @param app {EzyApp} App has just been created
+     * @param data {array} [appId, appName, data=[]]
      */
     postHandle(app, data) {
     }
@@ -241,8 +242,20 @@ export class EzyAppExitHandler {
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.APP_REQUEST command
+ * from server (response from server when clients send an APP_REQUEST request).
+ * This handler calls an appropriate app's handler to process corresponding app's command
+ * and data in the received server response.
+ *
+ * **Note**: This is an automatic handler, no need to manually define actions for it
+ */
 export class EzyAppResponseHandler {
+    /**
+     * Automatically call this function when client receives APP_REQUEST command
+     * from server
+     * @param data {array} [appId, responseData=[command, commandData]]
+     */
     handle(data) {
         var appId = data[0];
         var responseData = data[1];
@@ -263,8 +276,23 @@ export class EzyAppResponseHandler {
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.PLUGIN_INFO command
+ * from server, meaning that server allows client to access plugin
+ * - Usage:
+ * ```
+ *      let pluginInfoHandler = new Ezy.EzyPluginInfoHandler();
+ *      pluginInfoHandler.postHandle = function (plugin, data) {
+ *          // What to do after being allowed to access plugin
+ *      };
+ * ```
+ */
 export class EzyPluginInfoHandler {
+    /**
+     * Automatically call this function when client receives PLUGIN_INFO command
+     * from server
+     * @param data {array} [pluginId, pluginName, data=[]]
+     */
     handle(data) {
         var zone = this.client.zone;
         var pluginManager = zone.pluginManager;
@@ -273,7 +301,12 @@ export class EzyPluginInfoHandler {
         this.postHandle(plugin, data);
         Util.EzyLogger.console("request plugin: " + plugin.name + " info successfully");
     }
-
+    /**
+     * Create an EzyPlugin entity for client
+     * @param zone {EzyZone}
+     * @param data {array} [pluginId, pluginName, data=[]]
+     * @returns {EzyPlugin} Created plugin
+     */
     newPlugin(zone, data) {
         var pluginId = data[0];
         var pluginName = data[1];
@@ -281,13 +314,31 @@ export class EzyPluginInfoHandler {
         return plugin;
     }
 
+    /**
+     * This function need to be manually defined to specify what to
+     * do after server allows client to access the app
+     * @param plugin {EzyPlugin} Plugin has just been created
+     * @param data {array} [pluginId, pluginName, data=[]]
+     */
     postHandle(plugin, data) {
     }
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.PLUGIN_REQUEST command
+ * from server (response from server when clients send an PLUGIN_REQUEST request).
+ * This handler calls an appropriate plugin's handler to process corresponding plugin's command
+ * and data in the received server response.
+ *
+ * **Note**: This is an automatic handler, no need to manually define actions for it
+ */
 export class EzyPluginResponseHandler {
+    /**
+     * Automatically call this function when client receives PLUGIN_REQUEST command
+     * from server
+     * @param data {array} [pluginId, responseData=[command, commandData]]
+     */
     handle(data) {
         var pluginId = data[0];
         var responseData = data[1];
@@ -304,27 +355,46 @@ export class EzyPluginResponseHandler {
 }
 
 //======================================
-
+/**
+ * A listener which fires when client receives Const.EzyCommand.PONG command
+ * from server
+ */
 export class EzyPongHandler {
     handle(client) {
     }
 }
 
 //======================================
-
+/**
+ * Manager class of all data handlers of a zone. A data handler is a listener
+ * that fires when server send a command **with data** to client.
+ */
 export class EzyDataHandlers {
+    /**
+     * @param client {EzyClient}
+     */
     constructor(client) {
         this.handlers = {};
         this.client = client;
         this.pingSchedule = client.pingSchedule;
     }
 
+    /**
+     * Map a handler to a specific command
+     * @param cmd {{name: string, id: number}}
+     * @param handler
+     */
     addHandler(cmd, handler) {
         handler.client = this.client;
         handler.pingSchedule = this.pingSchedule;
         this.handlers[cmd.id] = handler;
     }
 
+    /**
+     * Get a handle by command
+     * @param cmd {{name: string, id: number}}
+     * @returns {*}
+     */
     getHandler(cmd) {
         var handler = this.handlers[cmd.id];
         return handler;
@@ -332,17 +402,29 @@ export class EzyDataHandlers {
 }
 
 //======================================
-
+/**
+ * Manager class of all data handlers of an app
+ */
 export class EzyAppDataHandlers {
 
     constructor() {
         this.handlers = {}
     }
 
+    /**
+     * Map a user-defined handler to a user-defined command
+     * @param cmd {string} User-defined command
+     * @param handler {function} User-defined handler
+     */
     addHandler(cmd, handler) {
         this.handlers[cmd] = handler;
     }
 
+    /**
+     * Get handler by command
+     * @param cmd {string}
+     * @returns {function}
+     */
     getHandler(cmd) {
         var handler = this.handlers[cmd];
         return handler;
@@ -351,17 +433,29 @@ export class EzyAppDataHandlers {
 }
 
 //======================================
-
+/**
+ * Manager class of all data handlers of a plugin.
+ */
 export class EzyPluginDataHandlers {
 
     constructor() {
         this.handlers = {}
     }
 
+    /**
+     * Map a user-defined handler to a user-defined command
+     * @param cmd {string} User-defined command
+     * @param handler {function} User-defined handler
+     */
     addHandler(cmd, handler) {
         this.handlers[cmd] = handler;
     }
 
+    /**
+     * Get handler by command
+     * @param cmd {string}
+     * @returns {function}
+     */
     getHandler(cmd) {
         var handler = this.handlers[cmd];
         return handler;
